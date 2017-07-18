3431d56da4b6a9fcc6ef6401558f5525b885c0a3
Dylan Brown <dylandoesprogramming@gmail.com>
2017-07-17 23:40:36 -0500

    Project mostly finished - I would like to add a swipe to remove feature but am struggling with the animation - double tap to remove for now

--- www/js/todoCtrl.js
+++ www/js/todoCtrl.js
@@ -1,7 +1,16 @@
 angular.module('myTodos').controller("todoCtrl",function($scope){
   $scope.todos = [];
+  $scope.input = {};
   $scope.pushTodo = function(todoBox){
-    $scope.todos.push(todoBox);
-    console.log($scope.todos);
+    if(todoBox){
+      $scope.todos.push(todoBox);
+      console.log(todoBox);
+      $scope.input.todoBox = "";
+    }
   }
+  $scope.removeTodo = function(index){
+    $scope.todos.splice(index,1);
+  }
+
+
 })
